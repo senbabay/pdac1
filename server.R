625a0f41c36abb946eadb0034d662ac6bfd4fd42
Yasin Senbabaoglu <shenbaba@gmail.com>
2016-05-31 14:02:39 -0400

    Add files via upload

--- server.R
+++ server.R
@@ -1,5 +1,30 @@
 library(shiny)
+###################################################################
+mat = readRDS("Expression_affy_v2_data_matrix.rds")
+zmat = t(scale(t(mat)))
+immune = readRDS("immune_score_matrix_vZScoreAverages.rds")
+### Unsupervised clustering
+vars = apply(mat,1,sd)^2
+# Threshold vector
+lx = seq(3,50,1) / 10
 
+pve = len = rep(NA,length(lx))
+
+for(i in 1:length(lx)){
+  #print(i)
+  SDCUT = lx[i]
+  len[i] = length(which(vars > SDCUT))
+  
+  zmat2 = zmat[which(vars > SDCUT),]
+  pc = prcomp(t(zmat2))
+  x3 = pc$x[,1:3]
+  pve[i] = sum(apply(x3,2,sd)^2) / sum(apply(pc$x,2,sd)^2)
+}#end for i
+
+resp = as.factor(sapply(strsplit(colnames(zmat),"_"),"[",c(F,T)))
+respCol = rep("red",length(resp))
+respCol[which(resp=="LT")] = "turquoise4"
+###################################################################
 shinyServer(function(input, output) { 
   
   varcut <- reactive({
